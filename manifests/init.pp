b7d749b90dcad1ba9319b4434934d2ae21fddb67
Michael Stahnke <stahnma@puppetlabs.com>
2013-05-10 10:09:54 -0700

    Simplify and update to 2.0.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,10 @@
 # Public: Install Apache Directory Studio.app to /Applications.
 #
 # Examples
-#
-#   include vlc
-class apachedirectorystudio(
-  $version = $apachedirectorystudio::params::default_version
-) inherits apachedirectorystudio::params {
-
-  $version_clean = regsubst($version, '\.', '_', 'G')
+class apachedirectorystudio {
 
   package { 'apachedirectorystudio':
     provider => 'appdmg',
-    source   => $apachedirectorystudio::params::versions[$version_clean],
+    source   => 'http://apache.mirrors.tds.net//directory/studio/dist/2.0.0.v20130308/ApacheDirectoryStudio-macosx-x86_64-2.0.0.v20130308.dmg'
   }
 }
